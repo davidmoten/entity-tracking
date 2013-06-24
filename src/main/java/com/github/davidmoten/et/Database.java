8365cf664e7d43cd04abafb37c8c99f38aa6a075
Dave Moten <davidmoten@gmail.com>
2013-06-24 10:04:55 +1000

    work on getReports db method

--- src/main/java/com/github/davidmoten/et/Database.java
+++ src/main/java/com/github/davidmoten/et/Database.java
@@ -20,6 +20,8 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 /**
  * Encapsulates database access. GoogleAppEngine (BigTable) used for
@@ -69,16 +71,17 @@ public class Database {
 		System.out.println("saved");
 	}
 
-	public String getReports(String user, double topLeftLat, double topLeftLon,
-			double bottomRightLat, double bottomRightLon, Date start,
-			Date finish, String idName, String idValue) {
+	public String getReports(String user, final double topLeftLat,
+			final double topLeftLon, final double bottomRightLat,
+			final double bottomRightLon, Date start, Date finish,
+			String idName, String idValue) {
 
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
 		Filter startTimeFilter = new FilterPredicate("time",
 				FilterOperator.GREATER_THAN_OR_EQUAL, start);
 		Filter finishTimeFilter = new FilterPredicate("time",
-				FilterOperator.GREATER_THAN_OR_EQUAL, finish);
+				FilterOperator.LESS_THAN, finish);
 		Filter userFilter = new FilterPredicate("user", FilterOperator.EQUAL,
 				user);
 		Filter filter = CompositeFilterOperator.and(userFilter,
@@ -96,9 +99,30 @@ public class Database {
 		Query query = new Query("Report").setFilter(filter);
 		FetchOptions options = FetchOptions.Builder.withChunkSize(1000);
 		Iterable<Entity> it = datastore.prepare(query).asIterable(options);
+		it = Iterables.filter(it, new Predicate<Entity>() {
+			@Override
+			public boolean apply(Entity e) {
+				double lat = (double) e.getProperty("lat");
+				double lon = (double) e.getProperty("lon");
+				// TODO handle longitude between
+				return lat >= bottomRightLat && lat <= topLeftLat
+						&& longitudeBetween(topLeftLon, bottomRightLon, lon);
+			}
 
+		});
 		// filter results within bounding box because geohash query inexact.
+
 		return toJSON(it);
+	}
+
+	private static boolean longitudeBetween(double a, double b, double c) {
+		// TODO implement
+		a = GeoHash.to180(a);
+		b = GeoHash.to180(b);
+		c = GeoHash.to180(c);
+		if (b < a)
+			return longitudeBetween(a, b + 360, c);
+		return a <= c && c <= b;
 	}
 
 	private String toJSON(Iterable<Entity> it) {
